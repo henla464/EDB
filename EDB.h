05566917627c51e74b48d9f9ab9eb6956d1ac190
Henrik Larsson <laselase@gmail.com>
2016-09-12 20:10:54 +0200

    Update EDB.h

--- EDB.h
+++ EDB.h
@@ -1,27 +1,40 @@
 /*
   EDB.h
   Extended Database Library for Arduino
+
   http://www.arduino.cc/playground/Code/ExtendedDatabaseLibrary
+
+  Based on code from:
+  Database library for Arduino 
+  Written by Madhusudana das
+  http://www.arduino.cc/playground/Code/DatabaseLibrary
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #ifndef EDB_PROM
 #define EDB_PROM
-#define EDB_FLAG B11011011
 
 struct EDB_Header
 {
-  byte flag;
   unsigned long n_recs;
   unsigned int rec_size;
   unsigned long table_size;
 };
 
-typedef enum EDB_Status { 
-                          EDB_OK,
-                          EDB_ERROR,
-                          EDB_OUT_OF_RANGE,
-                          EDB_TABLE_FULL
-                        };
+enum EDB_Status { EDB_OK, EDB_OUT_OF_RANGE, EDB_TABLE_FULL, EDB_CORRUPT };
 
 typedef byte* EDB_Rec;
 
@@ -33,14 +46,14 @@ class EDB {
     typedef uint8_t EDB_Read_Handler(unsigned long);
     EDB(EDB_Write_Handler *, EDB_Read_Handler *);
     EDB_Status create(unsigned long, unsigned long, unsigned int);
-    EDB_Status open(unsigned long);
+    EDB_Status open(unsigned long, unsigned long, unsigned int);
     EDB_Status readRec(unsigned long, EDB_Rec);
     EDB_Status deleteRec(unsigned long);	
     EDB_Status insertRec(unsigned long, const EDB_Rec);
     EDB_Status updateRec(unsigned long, const EDB_Rec);
     EDB_Status appendRec(EDB_Rec rec);
     unsigned long limit();
-    unsigned long count();
+	  unsigned long count();
     void clear();
   private:
     unsigned long EDB_head_ptr;
